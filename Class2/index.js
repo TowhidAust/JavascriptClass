4eff6d964d246ae7cf35953b3fbfb0526cf48801
TowhidAust <007towhid2016@gmail.com>
2020-09-07 22:28:13 +0600

    class2

---
+++ Class2/index.js
@@ -0,0 +1,61 @@
+// how to change html contents with javascript
+// 1. document.getElementById
+// 2. document.getElementsByClassName
+// css # for id
+
+document.getElementById("htmlHeaderTag").innerHTML = "This is javascript inner html";
+console.log("Hello world");
+// window.alert("Hello world");
+
+document.write("this is document write");
+
+
+// statement
+// let const var
+
+// declare multiple variable 
+let a, b, c;
+
+let x, y, z;
+x = y + z; // statement
+
+
+// let a;
+// let b;
+// let c;
+
+//what function
+
+// code blocks?
+
+function myFunction() {
+    // code block
+    console.log("Hi this is myFunction Codeblock");
+}
+
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+
+
+// let number1 = 10;
+// let number2 = 20;
+
+// create a function calculator
+function calculator(number1, number2) {
+    let result;
+    result = number1 + number2;
+    console.log("The added result is", result);
+}
+
+calculator(10, 20);
+
+// Assignment 1
+// Create a subtraction(biyog) calculator
+// Multiplication (gun)
+// division (vaag)
+
+
+// Assignment 2
+// Celcius to farrenheit calculator with a function
